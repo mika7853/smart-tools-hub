7ecc9043a7a1b2d38f973a9ffee0d9b7729bf91a
mika7853 <mikadigal508@gmail.com>
2026-07-31 20:05:58 +0530

    Initialize React application in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,9 @@
-App.tsx
-src/App.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
